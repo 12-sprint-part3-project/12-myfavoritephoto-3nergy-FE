d0d054f23e638e8f782f0ae129ba5404c61fc627
yooseohyeon <yooseohyeon.02@gmail.com>
2026-06-04 14:18:55 +0900

    chore: Storybook Baskin 폰트 설정 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,6 @@ const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
   framework: '@storybook/nextjs',
-  staticDirs: ['../public'],
+  staticDirs: ['../public', '../src/app/fonts'],
 };
 export default config;
