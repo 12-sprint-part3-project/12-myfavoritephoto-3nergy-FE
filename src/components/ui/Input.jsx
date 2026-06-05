9e6569c12c33ce9864d1251665ab57b1c973a381
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-06-05 16:33:01 +0900

    feat: Form UI 공통 Input 컴포넌트

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -28,7 +28,10 @@ const Input = ({
   return (
     <div className="flex flex-col gap-[.625rem]">
       {label && (
-        <label htmlFor={inputId} className={`${disabled && 'text-gray-200'}`}>
+        <label
+          htmlFor={inputId}
+          className={`${disabled ? 'text-gray-400' : 'text-white'}`}
+        >
           {label}
         </label>
       )}
@@ -44,20 +47,16 @@ const Input = ({
           required={required}
           disabled={disabled}
           {...rest}
-          className={`focus:border-main focus:invalid:border-red invalid:border-red text-noto-14-regular md:text-noto-16-regular h-[3.4375rem] w-full rounded-xs border border-white bg-black px-5 text-white outline-none placeholder:text-gray-200 focus:border md:h-[3.75rem] ${error && 'border-red'}`}
+          className={`focus:invalid:border-red invalid:border-red text-noto-14-regular md:text-noto-16-regular h-[3.4375rem] w-full rounded-xs border bg-black px-5 text-white outline-none placeholder:text-gray-200 focus:border disabled:placeholder:text-gray-300 md:h-[3.75rem] ${error ? 'border-red focus:border-red' : 'focus:border-main border-white'}`}
         />
 
         {isPassword && (
           <button
             type="button"
-            onClick={() => setShowPassword((prev) => !prev)}
-            className="absolute top-[50%] right-5 h-[1.5rem] w-[1.5rem] -translate-y-[50%]"
+            onClick={() => setShowPassword((prev) => (disabled ? prev : !prev))}
+            className={`absolute top-[50%] right-5 h-[1.5rem] w-[1.5rem] -translate-y-[50%] ${disabled ? 'text-gray-400' : 'cursor-pointer text-white'}`}
           >
-            {showPassword ? (
-              <Invisible width={24} height={24} fill="white" />
-            ) : (
-              <Visible width={24} height={24} fill="white" />
-            )}
+            {showPassword ? <Invisible /> : <Visible />}
             <span className="sr-only">{showPassword ? '숨기기' : '보기'}</span>
           </button>
         )}
