9b5eada2993d0d78ccdcd64f34a787151024036f
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-06-05 14:06:01 +0900

    feat: Form UI 공통Input 컴포넌트

---
+++ src/components/ui/Input.jsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useId, useState } from 'react';
+import Visible from '@/icons/visible.svg';
+import Invisible from '@/icons/invisible.svg';
+
+const Input = ({
+  label,
+  name,
+  type = 'text',
+  value,
+  placeholder,
+  onChange,
+  required = false,
+  disabled = false,
+  error,
+  hint,
+  icon,
+  ...rest
+}) => {
+  const uid = useId();
+  const inputId = `${name}-${uid}`;
+
+  const [showPassword, setShowPassword] = useState(false);
+  const isPassword = type === 'password';
+  const passwordTypeToggle = isPassword && showPassword ? 'text' : type;
+
+  return (
+    <div className="flex flex-col gap-[.625rem]">
+      {label && (
+        <label htmlFor={inputId} className={`${disabled && 'text-gray-200'}`}>
+          {label}
+        </label>
+      )}
+
+      <div className="relative">
+        <input
+          type={passwordTypeToggle}
+          name={name}
+          id={inputId}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          required={required}
+          disabled={disabled}
+          {...rest}
+          className={`focus:border-main focus:invalid:border-red invalid:border-red text-noto-14-regular md:text-noto-16-regular h-[3.4375rem] w-full rounded-xs border border-white bg-black px-5 text-white outline-none placeholder:text-gray-200 focus:border md:h-[3.75rem] ${error && 'border-red'}`}
+        />
+
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="absolute top-[50%] right-5 h-[1.5rem] w-[1.5rem] -translate-y-[50%]"
+          >
+            {showPassword ? (
+              <Invisible width={24} height={24} fill="white" />
+            ) : (
+              <Visible width={24} height={24} fill="white" />
+            )}
+            <span className="sr-only">{showPassword ? '숨기기' : '보기'}</span>
+          </button>
+        )}
+      </div>
+
+      {error && <p className="text-noto-16-light text-red">{error}</p>}
+    </div>
+  );
+};
+
+export default Input;
