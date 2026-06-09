955b5d632a9726e0e6d9cadab5cb983d22da58ae
yooseohyeon <yooseohyeon.02@gmail.com>
2026-06-09 10:19:18 +0900

    feat: Provider 통합 및 루트 layout에 적용

---
+++ src/app/providers/providers.js
@@ -0,0 +1,13 @@
+'use client';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/react-query/queryClient';
+import { AuthProvider } from '@/context/AuthContext';
+
+export default function Providers({ children }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>{children}</AuthProvider>
+    </QueryClientProvider>
+  );
+}
