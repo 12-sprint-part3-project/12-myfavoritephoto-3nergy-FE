d15ef8e10430319c56902748fadeb08b3216db4b
NAYA3 <popcorn7274@gmail.com>
2026-06-10 11:57:22 +0900

    fix: ProfileMenu 스토리북 docs 렌더링 문제 수정

--- src/components/layout/GNB/ProfileMenu.stories.jsx
+++ src/components/layout/GNB/ProfileMenu.stories.jsx
@@ -9,6 +9,19 @@ const meta = {
   title: 'Layout/GNB/ProfileMenu',
   component: ProfileMenu,
   tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+    docs: {
+      story: {
+        inline: false,
+        height: '500px',
+      },
+      description: {
+        component:
+          'GNB 프로필 메뉴입니다. PC/태블릿(≥640px)에서는 우측 상단 드롭다운으로, 모바일(<640px)에서는 좌측 전체 높이 드로어로 표시됩니다. 스토리북 툴바의 뷰포트를 조절해 두 가지 레이아웃을 확인할 수 있습니다.',
+      },
+    },
+  },
   argTypes: {
     user: {
       control: 'object',
@@ -22,15 +35,8 @@ const meta = {
 
 export default meta;
 
-export const Dropdown = {
-  name: 'PC/태블릿 드롭다운',
-  args: {
-    user: MOCK_USER,
-  },
-};
-
-export const Drawer = {
-  name: '모바일 드로어',
+export const Default = {
+  name: '기본',
   args: {
     user: MOCK_USER,
   },
