ae1a9edb6760153a09231702bbb47c0ca4c9bfde
yooseohyeon <yooseohyeon.02@gmail.com>
2026-06-10 09:47:11 +0900

    feat: QUERY_KEYS 상수 추가

---
+++ src/constants/queryKeys.js
@@ -0,0 +1,32 @@
+// constants/queryKeys.js
+export const QUERY_KEYS = {
+  // 유저
+  me: ['me'],
+
+  // 알림
+  notifications: {
+    all: ['notifications'],
+  },
+
+  // 포토카드
+  photocards: {
+    all: ['photocards'],
+    detail: (id) => ['photocards', id],
+  },
+
+  // 판매 (마켓플레이스)
+  sales: {
+    all: ['sales'],
+    detail: (id) => ['sales', id],
+  },
+
+  // 나의 판매 카드
+  mySales: {
+    all: ['mySales'],
+  },
+
+  // 보유 카드 (마이갤러리)
+  myGallery: {
+    all: ['myGallery'],
+  },
+};
