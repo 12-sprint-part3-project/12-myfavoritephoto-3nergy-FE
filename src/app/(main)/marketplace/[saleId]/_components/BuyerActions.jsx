d9da62fe5efdcacf67c8a937425bc316ac7388c0
yooseohyeon <yooseohyeon.02@gmail.com>
2026-06-10 16:33:38 +0900

    feat: BuyerActions 컴포넌트 구현

---
+++ src/app/(main)/marketplace/[saleId]/_components/BuyerActions.jsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@/components/ui/Button';
+import { CounterInput } from '@/components/ui/CounterInput';
+
+export const BuyerActions = ({ sale }) => {
+  const [value, setValue] = useState(1);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+
+  const totalPrice = value * sale.price;
+
+  return (
+    <div className="mt-[1.875rem] flex flex-col">
+      {/* 구매 수량 + 총 가격 */}
+      <div className="flex flex-col gap-[1.25rem]">
+        <CounterInput
+          label="구매 수량"
+          labelClassName="text-noto-18-regular lg:text-noto-20-regular"
+          value={value}
+          onChange={setValue}
+          min={1}
+          max={sale.remainingQuantity}
+        />
+        <div className="flex justify-between">
+          <span className="text-noto-18-regular lg:text-noto-20-regular text-white">
+            총 가격
+          </span>
+          <span className="flex items-end gap-[0.62rem]">
+            <span className="text-noto-20-bold lg:text-noto-24-bold text-white">
+              {totalPrice.toLocaleString()}P
+            </span>
+            <span className="text-noto-18-regular lg:text-noto-20-regular text-gray-300">
+              ({sale.remainingQuantity}장)
+            </span>
+          </span>
+        </div>
+      </div>
+      <div className="mt-10 lg:mt-[5rem]">
+        <Button
+          size="thick"
+          className="text-noto-18-bold lg:text-noto-20-bold w-full"
+          onClick={() => setShowConfirmModal(true)}
+        >
+          포토카드 구매하기
+        </Button>
+      </div>
+
+      {/*
+      {showConfirmModal && (
+        <BuyConfirmModal onClose={() => setShowConfirmModal(false)} />
+      )}
+      */}
+    </div>
+  );
+};
