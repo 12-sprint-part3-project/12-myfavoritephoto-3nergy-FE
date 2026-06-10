5e212f2fb7825a40ff724734d53382f02ce76b97
yooseohyeon <yooseohyeon.02@gmail.com>
2026-06-10 18:14:51 +0900

    feat: (main)/layout.js에 GNB 추가 (내 정보 조회 API 연동 전)

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,8 +1,25 @@
+'use client';
+
+import { useAuth } from '@/context/AuthContext';
+//import { useMe } from '@/hooks/user/useMe';
+import { GNB } from '@/components/layout/GNB/GNB';
+
 export default function MainLayout({ children }) {
+  const { accessToken, logout } = useAuth();
+
+  /*
+  // TODO: 내 정보 조회 API 연동
+  const { data: user } = useMe({
+    enabled: !!accessToken,
+  });
+  */
+
   return (
-    <div className="flex min-h-screen flex-col">
-      {/* <GNB /> */}
-      <main className="mx-auto w-full max-w-[1480px] px-[15px] pt-[20px] md:px-[20px] md:pt-[40px] xl:pt-[60px]">
+    <div className="flex flex-col">
+      {/* TODO: user={user} 추가 */}
+      <GNB isAuthenticated={!!accessToken} onLogout={logout} />
+
+      <main className="mx-auto w-full max-w-[1480px] px-[0.9375rem] md:px-[1.25rem]">
         {children}
       </main>
     </div>
