c1654b0ab8d1de28ac0fa9787779428e3af4f8da
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-06-14 01:44:09 +0900

    refactor: 등급별 수량 현황 컴포넌트 className props 추가

--- src/components/domain/photocard/GradeBadgeList.jsx
+++ src/components/domain/photocard/GradeBadgeList.jsx
@@ -1,15 +1,13 @@
 import { GRADE_STYLE } from '@/constants/card';
-import GradeBadge from './GradeBadge';
+import GradeBadge from '@/components/domain/photocard/GradeBadge';
 
-const GradeBadgeList = ({ grades }) => {
+const GradeBadgeList = ({ grades, className = '' }) => {
   return (
-    <div className="flex flex-nowrap items-center gap-[10px] overflow-x-auto md:gap-5">
+    <div
+      className={`flex flex-nowrap items-center gap-[10px] overflow-x-auto md:gap-5 ${className}`}
+    >
       {grades.map(({ grade, count }) => (
-        <GradeBadge
-          key={grade}
-          count={count}
-          {...GRADE_STYLE[grade]}
-        />
+        <GradeBadge key={grade} count={count} {...GRADE_STYLE[grade]} />
       ))}
     </div>
   );
