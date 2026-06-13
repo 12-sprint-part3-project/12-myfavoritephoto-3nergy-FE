749748d219349b7ac76d2d98d041627eb1c20f98
yooseohyeon <yooseohyeon.02@gmail.com>
2026-06-13 18:27:45 +0900

    docs: SearchBar 스토리에 너비 조절 설명 추가

--- src/components/ui/SearchBar.stories.jsx
+++ src/components/ui/SearchBar.stories.jsx
@@ -5,10 +5,37 @@ const meta = {
   component: SearchBar,
   argTypes: {},
   tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: '너비는 `w-full`이므로 래퍼 요소의 크기로 조절합니다.',
+      },
+    },
+  },
 };
 
 export default meta;
 
 export const Default = {
   args: {},
+};
+
+export const WithWrapper = {
+  name: '너비 조절 예시',
+  render: () => (
+    <div className="w-[320px]">
+      <SearchBar />
+    </div>
+  ),
+  parameters: {
+    docs: {
+      source: {
+        code: `
+<div className="w-[320px]">
+  <SearchBar />
+</div>
+        `,
+      },
+    },
+  },
 };
