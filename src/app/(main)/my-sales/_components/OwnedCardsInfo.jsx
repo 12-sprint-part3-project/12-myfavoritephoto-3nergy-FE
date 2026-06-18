7ecc2ea0b6d7137d922d93c500bc2c2374136d24
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-06-18 18:37:10 +0900

    style: 문구 수정

--- src/app/(main)/my-sales/_components/OwnedCardsInfo.jsx
+++ src/app/(main)/my-sales/_components/OwnedCardsInfo.jsx
@@ -2,7 +2,7 @@ export const OwnedCardsInfo = ({ nickname, allCardsCnt }) => {
   return (
     <div className="mb-[15px] flex items-center gap-[5px] md:mb-[20px] md:gap-[10px]">
       <span className="text-noto-14-bold text-gray-200 md:text-noto-20-bold lg:text-noto-24-bold">
-        {nickname}님이 보유한 포토카드
+        {nickname}님이 거래중인 포토카드
       </span>
       <span className="text-noto-12-regular text-gray-300 md:text-noto-18-regular lg:text-noto-20-regular">
         ({allCardsCnt}장)
