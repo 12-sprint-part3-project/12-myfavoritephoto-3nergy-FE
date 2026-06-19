5aca3cf0a33f8653dccd0bda91cbd0cb96cae627
yooseohyeon <yooseohyeon.02@gmail.com>
2026-06-19 09:33:43 +0900

    fix: usePurchaseSale에서 올바르게 판매 상태 가져오도록 수정

--- src/hooks/sale/usePurchaseSale.js
+++ src/hooks/sale/usePurchaseSale.js
@@ -8,15 +8,15 @@ export const usePurchaseSale = (saleId) => {
   return useMutation({
     mutationKey: ['purchaseSale'],
     mutationFn: (quantity) => purchaseSale(saleId, quantity),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.sales.all() }); // 판매 상태/잔여 수량 변동
+    onSuccess: (data) => {
+      queryClient.refetchQueries({ queryKey: QUERY_KEYS.sales.all() }); // 판매 상태/잔여 수량 변동
       queryClient.invalidateQueries({ queryKey: QUERY_KEYS.photocards.all() }); // 보유 포토카드 목록 (판매 등록, 교환 제안)
       queryClient.invalidateQueries({ queryKey: QUERY_KEYS.myGallery.all() }); // 구매자 마이갤러리
       queryClient.invalidateQueries({ queryKey: QUERY_KEYS.mySales.all() }); // 판매자 판매 목록
       queryClient.invalidateQueries({ queryKey: QUERY_KEYS.point.me() }); // 구매 시 포인트 차감
 
       // 품절된 경우 해당 판매의 교환 제안을 무효화
-      const { status } = data.data.sale;
+      const { status } = data.sale;
       if (status === 'SOLD_OUT') {
         queryClient.invalidateQueries({ queryKey: QUERY_KEYS.trades.all() });
       }
