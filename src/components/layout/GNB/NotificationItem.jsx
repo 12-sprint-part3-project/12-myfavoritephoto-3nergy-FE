21ef60ef4886fdf80c904309e5a016c7220c16cb
yooseohyeon <yooseohyeon.02@gmail.com>
2026-06-19 22:39:45 +0900

    feat: NotificationItem의 cursor를 pointer로 변경

--- src/components/layout/GNB/NotificationItem.jsx
+++ src/components/layout/GNB/NotificationItem.jsx
@@ -23,7 +23,7 @@ export const NotificationItem = ({
   return (
     <button
       onClick={handleClick}
-      className={`flex w-full flex-col gap-[0.62rem] border-b-1 border-gray-400 px-[1.25rem] pt-[1.25rem] pb-[1.63rem] text-left md:pb-[1.81rem] ${bgClass} ${className}`}
+      className={`flex w-full cursor-pointer flex-col gap-[0.62rem] border-b-1 border-gray-400 px-[1.25rem] pt-[1.25rem] pb-[1.63rem] text-left md:pb-[1.81rem] ${bgClass} ${className}`}
     >
       <p className={`text-noto-14-regular ${textClass}`}>{message}</p>
       <span className="text-noto-12-light text-gray-300">
