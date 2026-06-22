404f520a2d96fd3e57a76aa03696aba66b34a9fa
A <popcorn7274@gmail.com>
2026-06-22 15:10:12 +0900

    fix: 플로팅 버튼 모바일 하단 버튼 겹침 수정

--- src/components/layout/FloatingButtons.jsx
+++ src/components/layout/FloatingButtons.jsx
@@ -4,7 +4,7 @@ import { RandomPointButton } from '@/components/layout/RandomPointButton';
 import { ScrollTopButton } from '@/components/layout/ScrollTopButton';
 
 export const FloatingButtons = () => (
-  <div className="fixed bottom-[50px] right-[50px] z-40 flex flex-col items-center gap-2.5 md:gap-5">
+  <div className="fixed right-[0.9375rem] bottom-[100px] z-40 flex flex-col items-center gap-2.5 md:right-[1.25rem] md:bottom-[50px] md:gap-5">
     <RandomPointButton />
     <ScrollTopButton />
   </div>
