5b2ded9f274b5608467b07d4cc4a2501ed622546
A <popcorn7274@gmail.com>
2026-06-22 16:14:53 +0900

    fix: 알림/결과 페이지에서 플로팅 버튼 숨김 처리

--- src/components/layout/FloatingButtons.jsx
+++ src/components/layout/FloatingButtons.jsx
@@ -1,11 +1,40 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import { RandomPointButton } from '@/components/layout/RandomPointButton';
 import { ScrollTopButton } from '@/components/layout/ScrollTopButton';
 
-export const FloatingButtons = () => (
-  <div className="fixed right-[0.9375rem] bottom-[100px] z-40 flex flex-col items-center gap-2.5 md:right-[1.25rem] md:bottom-[50px] md:gap-5">
-    <RandomPointButton />
-    <ScrollTopButton />
-  </div>
-);
+const isHiddenPath = (pathname) => {
+  if (pathname === '/my-notifications') {
+    return true;
+  }
+
+  if (pathname === '/marketplace/create/result') {
+    return true;
+  }
+
+  if (/^\/marketplace\/[^/]+\/purchase\/result$/.test(pathname)) {
+    return true;
+  }
+
+  if (/^\/marketplace\/[^/]+\/trade\/result$/.test(pathname)) {
+    return true;
+  }
+
+  return false;
+};
+
+export const FloatingButtons = () => {
+  const pathname = usePathname();
+
+  if (isHiddenPath(pathname)) {
+    return null;
+  }
+
+  return (
+    <div className="fixed right-[0.9375rem] bottom-[100px] z-40 flex flex-col items-center gap-2.5 md:right-[1.25rem] md:bottom-[50px] md:gap-5">
+      <RandomPointButton />
+      <ScrollTopButton />
+    </div>
+  );
+};
